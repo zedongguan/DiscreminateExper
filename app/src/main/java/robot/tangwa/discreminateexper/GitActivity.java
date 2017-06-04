cda69a64f8b5466036f4b4816f33fd9cfdf7894c
zedongguan <zedong_guan@foxmail.com>
2017-06-04 23:29:29 +0800

    没上传上去，为什么呢？

---
+++ app/src/main/java/robot/tangwa/discreminateexper/GitActivity.java
@@ -0,0 +1,18 @@
+package robot.tangwa.discreminateexper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2017/6/4.
+ */
+
+public class GitActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
